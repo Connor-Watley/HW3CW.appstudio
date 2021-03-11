f8a6d181e902c3528e6ab78c5fa799ad7ce05324
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-10 23:51:34 -0600

    Update functionCompare.js

--- forms/functionCompare/functionCompare.js
+++ forms/functionCompare/functionCompare.js
@@ -5,11 +5,11 @@ function Compare(pass, confirmPass) {
     let confirmed = pass.localeCompare(confirmPass);
     return confirmed;
     }
-    
-let samePasswords = Compare(passWord, confirmPassWord)
 
 if(samePasswords = 0) {
 console.log("The passwords are the same")
 } else {
     console.log("The passwords are not the same")
     }
+    
+let samePasswords = Compare(passWord, confirmPassWord)

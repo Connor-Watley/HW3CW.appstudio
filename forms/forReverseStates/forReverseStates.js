7bed3775967fbb51d91ec0f6ad3f611932d0f6a0
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-10 23:32:18 -0600

    Update forReverseStates.js

--- forms/forReverseStates/forReverseStates.js
+++ forms/forReverseStates/forReverseStates.js
@@ -1,6 +1,6 @@
 let states = ["Nebraska", "Iowa", "Minnesota", "South Dakota", "North Dakota", "Kansas", "Oklahoma", "Texas"]
 
 for(i = 0; i < states.length; i++) {
-    states.reverse();
-    console.log(states[i])
+    let reversedStates = states.reverse();
+    console.log(reversedStates[i])
     }

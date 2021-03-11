b27b017847ec188a70b9e3d32199215d8f5495d0
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-10 22:55:26 -0600

    Create cityWhile.js

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,17 @@
+let cities = []
+let moreData = "yes"
+let i = 0
+
+while(moreData = "yes") {
+   let cityName = prompt("Enter a city name")
+   cityName.toLowerCase();
+   cities.push(cityName);
+   moreData = prompt("Enter yes to add more cities")
+   }
+
+while(i < cities.length) {
+    console.log(cities[i])
+    i++
+    }
+
+   

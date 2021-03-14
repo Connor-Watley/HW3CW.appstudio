5fb181ecebcfec6230b49e617811ad33db85a966
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-14 11:53:54 -0500

    Create dogStore.js

---
+++ forms/dogStore/dogStore.js
@@ -0,0 +1,16 @@
+let items = ["Crocheted Dog Toy", "Slow Feeder Bowl", "Pawz Grey Treat Jar", "Blue Sweet Dreams Thermal Pajamas", "Checker Chewy Vuiton Bowl", "Red Heart Sweater Dress", "Velvet Hair Bow - Sun Kissed", "Into the Wild Leash"]
+let price = [10.00, 14.00, 20.00, 30.00, 32.00, 27.00, 12.00 , 27.00 ]
+let wishList = []
+let userInput = Input1.value
+wishList.push(items[userInput])
+wishList.push(price[userInput])
+
+
+
+
+
+
+
+Button1.onclick=function(){
+  alert(`${wishList}`)
+}

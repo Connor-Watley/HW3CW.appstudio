dcf053ca498c0bc1d786c964bdd9127ea6387f2f
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-14 15:31:57 -0500

    Update functionCompare.js

--- forms/functionCompare/functionCompare.js
+++ forms/functionCompare/functionCompare.js
@@ -1,4 +1,4 @@
-/*
+
 let passWord = prompt("Enter your password")
 let confirmPassWord = prompt("Confirm your password")
 
@@ -14,4 +14,3 @@ console.log("The passwords are the same")
 } else {
     console.log("The passwords are not the same")
     }
-*/
